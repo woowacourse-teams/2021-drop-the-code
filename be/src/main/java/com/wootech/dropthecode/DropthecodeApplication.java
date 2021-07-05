77d15b56192733d8d2c350cbac31ae34fc8f6e81
Seed <hsik0225@gmail.com>
2021-07-05 13:56:55 +0900

    build: 초기 환경 세팅
    의존성 추가 및 .gitignore 추가

---
+++ be/src/main/java/com/wootech/dropthecode/DropthecodeApplication.java
@@ -0,0 +1,13 @@
+package com.wootech.dropthecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropthecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropthecodeApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/com/wootech/dropthecode/DropthecodeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wootech.dropthecode;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropthecodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

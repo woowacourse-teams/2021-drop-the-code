ca6ffaa83b0c49e4832011904ff4754c5330e40e
shinsehantan <shinsehantan@users.noreply.github.com>
2021-07-08 19:24:52 +0900

    feat: 헤더, 플렉스 박스 컴포넌트 구현
    - babel plugin styled components 설치
    
    Co-authored-by: 서지환 <seojihwan213@naver.com>

---
+++ frontend/src/components/Header/Header.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from "@storybook/react";
+
+import Header, { Props } from "./Header";
+
+export default {
+  title: "components/Header",
+  component: Header,
+} as Meta;
+
+const Template: Story<Props> = (args) => (
+  <Header {...args} />
+);
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  title: <div>로고</div>,
+  children: "내용물",
+};
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+
+import styled from "styled-components";
+
+import { LAYOUT } from '../../utils/constants/size';
+import { COLOR_TYPE } from '../../utils/constants/theme';
+import { FlexSpaceBetween } from '../shared/Flexbox/Flexbox';
+
+interface InnerProps {
+  themeColor?: COLOR_TYPE;  
+}
+
+const Inner = styled.header<InnerProps>`
+  background-color: ${({ theme, themeColor = "primary" }) => theme.color[themeColor].normal};
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+`
+
+export interface Props extends InnerProps {
+  title?: ReactNode;
+  children?: ReactNode;
+}
+
+const Header = ({ title, children }: Props) => {
+
+  return (
+    <Inner>
+      <FlexSpaceBetween css={{ maxWidth: LAYOUT.LG, width: '100%', height: '100%', alignItems: 'center', padding: '0 30px'}}>
+        {title}
+        <div>
+          {children}
+        </div>
+      </FlexSpaceBetween>
+    </Inner>
+  )
+}
+export default Header;

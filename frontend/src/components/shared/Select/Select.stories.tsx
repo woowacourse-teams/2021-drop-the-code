1a62bdacc590cb0c0b9505129ab5c2d10476a57c
shinsehantan <1219ssm@gmail.com>
2021-07-09 11:45:49 +0900

    셀렉트 컴포넌트 구현 (#21)
    * feat: 셀렉트 컴포넌트 구현
    
    Co-authored-by: 서지환 <seojihwan213@naver.com>
    
    * style: prettier 적용
    
    Co-authored-by: shinsehantan <shinsehantan@users.noreply.github.com>
    Co-authored-by: 서지환 <seojihwan213@naver.com>

---
+++ frontend/src/components/shared/Select/Select.stories.tsx
@@ -0,0 +1,22 @@
+import { Story, Meta } from "@storybook/react";
+
+import Select, { Props } from "./Select";
+
+export default {
+  title: "components/shared/Select",
+  component: Select,
+} as Meta;
+
+const Template: Story<Props> = (args) => <Select {...args} />;
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  children: (
+    <>
+      <option value="rec">추천순</option>
+      <option value="fir">1</option>
+      <option value="sec">2</option>
+    </>
+  ),
+};

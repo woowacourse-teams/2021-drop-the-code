18a5ca25733783ea5eb0d85a9dea94ea5c2b3443
shinsehantan <shinsehantan@users.noreply.github.com>
2021-07-09 16:14:07 +0900

    feat: 인풋 컴포넌트에 에러 메시지 추가
    - 고정된 width 제거
    - labelText, errorMessage 동적으로 렌더링되도록 수정

--- frontend/src/components/shared/Input/Input.stories.tsx
+++ frontend/src/components/shared/Input/Input.stories.tsx
@@ -7,15 +7,12 @@ export default {
   component: Input,
 } as Meta;
 
-const Template: Story<Props> = (args) => (
-  <div>
-    <Input {...args}/>
-  </div>
-);
+const Template: Story<Props> = (args) => <Input {...args} />;
 
 export const Basic = Template.bind({});
 
 Basic.args = {
   placeHolder: "타이틀을 입력해주세요.",
-  labelText: "타이틀"
+  labelText: "타이틀",
+  errorMessage: "30자 이내로 작성해주세요.",
 };

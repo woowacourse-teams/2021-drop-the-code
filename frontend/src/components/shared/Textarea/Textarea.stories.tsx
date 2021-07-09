088da0c1efbf178ffe0ce5eaab30f7de747defa6
shinsehantan <shinsehantan@users.noreply.github.com>
2021-07-09 16:29:29 +0900

    feat: 텍스트에리어 컴포넌트에 에러 메시지 추가
    - 고정된 width 제거
    - labelText, errorMessage 동적으로 렌더링되도록 수정

--- frontend/src/components/shared/Textarea/Textarea.stories.tsx
+++ frontend/src/components/shared/Textarea/Textarea.stories.tsx
@@ -7,15 +7,13 @@ export default {
   component: Textarea,
 } as Meta;
 
-const Template: Story<Props> = (args) => (
-  <Textarea {...args} />
-);
+const Template: Story<Props> = (args) => <Textarea {...args} />;
 
 export const Basic = Template.bind({});
 
 Basic.args = {
   placeHolder: "리뷰어 소개를 입력해주세요.",
-  labelText: "리뷰어 소개"
+  labelText: "리뷰어 소개",
 };
 
 export const Large = Template.bind({});
@@ -23,5 +21,6 @@ export const Large = Template.bind({});
 Large.args = {
   height: "500px",
   placeHolder: "리뷰어 소개를 입력해주세요.",
-  labelText: "리뷰어 소개"
+  labelText: "리뷰어 소개",
+  errorMessage: "5,000자 이내로 입력해주세요.",
 };
--- frontend/src/components/shared/Textarea/Textarea.tsx
+++ frontend/src/components/shared/Textarea/Textarea.tsx
@@ -1,36 +1,48 @@
 import { TextareaHTMLAttributes } from "react";
 
-import  styled  from 'styled-components';
+import styled from "styled-components";
 
 import { COLOR } from "../../../utils/constants/color";
 
 interface InnerProps {
   height?: string;
   labelText?: string;
-  placeHolder: string;
-} 
+  placeHolder?: string;
+  errorMessage?: string;
+}
 
 const Label = styled.label`
   display: block;
   margin-bottom: 10px;
-`
+`;
 
 const Inner = styled.textarea<InnerProps>`
-  width: 500px;
-  max-width: 500px;
-  ${({height}) => (height ? `height: ${height}` : '')};
+  width: 100%;
+  ${({ height }) => (height ? `height: ${height}` : "")};
   padding: 5px;
   border-radius: 4px;
   line-height: 1.5;
   border: 1px solid ${COLOR.GRAY_500};
-`
+`;
+
+const ErrorMessage = styled.div`
+  padding: 5px;
+  font-size: 13px;
+  color: ${COLOR.RED_600};
+`;
 
 export type Props = TextareaHTMLAttributes<HTMLTextAreaElement> & InnerProps;
 
-const Textarea = ({labelText, ...props}: Props) => 
-<div>
-  <Label htmlFor={labelText}>{labelText}</Label>
-  <Inner id={labelText} {...props}/>;
-</div>
+const Textarea = ({ labelText, errorMessage, ...props }: Props) => {
+  const innerProps = { ...(labelText && { id: labelText }), ...props };
+
+  return (
+    <>
+      {labelText && <Label htmlFor={labelText}>{labelText}</Label>}
+      <Inner {...innerProps} />
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </>
+  );
+};
 
 export default Textarea;

6879882a566ae81a631a506ba907f4f5d342e359
kimjunseo <air.junseo@gmail.com>
2021-07-12 21:07:14 +0900

    refactor: OAuth 서버에 토큰 요청 기능 수정

--- backend/src/main/java/com/wootech/dropthecode/config/auth/controller/OauthController.java
+++ backend/src/main/java/com/wootech/dropthecode/config/auth/controller/OauthController.java
@@ -1,20 +1,19 @@
 package com.wootech.dropthecode.config.auth.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wootech.dropthecode.config.auth.dto.AuthorizationCode;
 import com.wootech.dropthecode.config.auth.dto.LoginResponse;
 import com.wootech.dropthecode.config.auth.dto.TokenRequest;
+import com.wootech.dropthecode.config.auth.dto.TokenResponse;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class OauthController {
@@ -25,29 +24,19 @@ public class OauthController {
     @Value("${oauth2.github.client_secret}")
     private String clientSecret;
 
-    private final ObjectMapper objectMapper;
-
-    public OauthController(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @PostMapping("/login/oauth")
-    public ResponseEntity<LoginResponse> login(@RequestBody AuthorizationCode authorizationCode) throws JsonProcessingException {
+    public ResponseEntity<LoginResponse> login(@RequestBody AuthorizationCode authorizationCode) {
         // TODO: client_id, client_secret, authorizationCode를 이용해서 토큰 요청하기
         TokenRequest tokenRequest = new TokenRequest(clientId, clientSecret, authorizationCode.getAuthorizationCode());
 
-        WebClient client = WebClient.builder()
-                                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                                    .build();
-
-        String jsonString = objectMapper.writeValueAsString(tokenRequest);
-
-        String accessToken = client.post()
-                                   .uri("https://github.com/login/oauth/access_token")
-                                   .accept(MediaType.APPLICATION_JSON)
-                                   .body(BodyInserters.fromValue(jsonString))
-                                   .retrieve()
-                                   .bodyToMono(String.class).block();
+        TokenResponse accessToken = WebClient.create()
+                                             .post()
+                                             .uri("https://github.com/login/oauth/access_token")
+                                             .contentType(MediaType.APPLICATION_JSON)
+                                             .accept(MediaType.APPLICATION_JSON)
+                                             .body(Mono.just(tokenRequest), TokenRequest.class)
+                                             .retrieve()
+                                             .bodyToMono(TokenResponse.class).block();
 
         // TODO: access token으로 name, email, avatar_url 요청
 

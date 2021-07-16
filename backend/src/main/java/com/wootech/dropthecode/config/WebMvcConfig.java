4c97cce6807eff7528cb324c84600cfaf8ed51d0
Seed <hsik0225@gmail.com>
2021-07-16 22:40:16 +0900

    fix: credentials 설정 해제

--- backend/src/main/java/com/wootech/dropthecode/config/WebMvcConfig.java
+++ backend/src/main/java/com/wootech/dropthecode/config/WebMvcConfig.java
@@ -1,14 +1,20 @@
 package com.wootech.dropthecode.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public PageableHandlerMethodArgumentResolverCustomizer pageableCustomizer() {
-        return resolver -> resolver.setOneIndexedParameters(true);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTION")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .exposedHeaders("*");
+
+        WebMvcConfigurer.super.addCorsMappings(registry);
     }
 }

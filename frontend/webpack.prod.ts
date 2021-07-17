989b9f3606f858d36a559e0a44d6294eaeea54cf
seojihwan <seojihwan213@naver.com>
2021-07-17 14:44:05 +0900

    chore: 중복 세팅 제거 및 axios 추가
    - 환경변수 이름 변경
    - lock.json storybook 버전 올바르게 수정
    
    Co-authored-by: shinsehantan <shinsehantan@users.noreply.github.com>

--- frontend/webpack.prod.ts
+++ frontend/webpack.prod.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import webpack from "webpack";
 import merge from "webpack-merge";
 
 import common from "./webpack.common";
@@ -8,6 +9,7 @@ dotenv.config({ path: ".env.production" });
 const config = merge(common, {
   mode: "production",
   devtool: false,
+  plugins: [new webpack.EnvironmentPlugin(["GITHUB_OAUTH_CLIENT_ID", "CLIENT_BASE_URL", "SERVER_BASE_URL"])],
 });
 
 export default config;

bc0d2092c9808998ce43734508924f3c08f0eee5
Jinho <jh8579@gmail.com>
2021-07-20 22:53:46 +0900

    feat: 필요한 repository interface 추가

--- backend/src/main/java/com/wootech/dropthecode/repository/TeacherProfileRepository.java
+++ backend/src/main/java/com/wootech/dropthecode/repository/TeacherProfileRepository.java
@@ -2,6 +2,7 @@ package com.wootech.dropthecode.repository;
 
 import java.util.List;
 
+import com.wootech.dropthecode.domain.Member;
 import com.wootech.dropthecode.domain.TeacherProfile;
 
 import org.springframework.data.domain.Page;
@@ -9,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherProfileRepository extends JpaRepository<TeacherProfile, Long> {
+
+    Boolean existsTeacherProfileByMember(Member member);
 
     Page<TeacherProfile> findDistinctAllByLanguagesLanguageNameAndSkillsSkillNameInAndCareerGreaterThanEqual(Pageable pageable, String languageName, List<String> skills, Integer career);
 }

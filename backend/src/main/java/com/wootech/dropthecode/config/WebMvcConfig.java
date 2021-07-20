3ac2c8a7e90e4aed32d7d65f39865770453fd033
kimjunseo <air.junseo@gmail.com>
2021-07-20 17:17:34 +0900

    feat: JWT 토큰 검증 인터셉터 추가 & 검증 관련 예외 생성

--- backend/src/main/java/com/wootech/dropthecode/config/WebMvcConfig.java
+++ backend/src/main/java/com/wootech/dropthecode/config/WebMvcConfig.java
@@ -1,11 +1,23 @@
 package com.wootech.dropthecode.config;
 
+import com.wootech.dropthecode.config.auth.AuthenticationInterceptor;
+import com.wootech.dropthecode.config.auth.GetAuthenticationInterceptor;
+import com.wootech.dropthecode.config.auth.service.AuthService;
+import com.wootech.dropthecode.config.auth.util.JwtTokenProvider;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final AuthService authService;
+
+    public WebMvcConfig(AuthService authService) {
+        this.authService = authService;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -17,5 +29,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .exposedHeaders("*");
 
         WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthenticationInterceptor(authService))
+                .addPathPatterns("/teachers", "/reviews", "/reviews/**");
+        registry.addInterceptor(new GetAuthenticationInterceptor(authService))
+                .addPathPatterns("/reviews/student/**");
     }
 }

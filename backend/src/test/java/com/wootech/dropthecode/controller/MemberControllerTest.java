ee1bee7cfd9718c2ce17a6db851932b050cb522a
Jinho <jh8579@gmail.com>
2021-07-21 02:35:41 +0900

    fix: 리뷰어 등록 API 문서에 필요 없는 부분 삭제

--- backend/src/test/java/com/wootech/dropthecode/controller/MemberControllerTest.java
+++ backend/src/test/java/com/wootech/dropthecode/controller/MemberControllerTest.java
@@ -49,7 +49,10 @@ public class MemberControllerTest extends RestApiDocumentTest {
     @DisplayName("리뷰어 등록 테스트 - 성공")
     @Test
     void registerTeacherTest() throws Exception {
-        List<TechSpec> techSpecs = Arrays.asList(new TechSpec("java", Arrays.asList("Spring", "Servlet")));
+        List<TechSpec> techSpecs = Arrays.asList(
+                new TechSpec("java", Arrays.asList("spring", "servlet")),
+                new TechSpec("javascript", Arrays.asList("vue", "react"))
+        );
         TeacherRegistrationRequest request
                 = new TeacherRegistrationRequest("백엔드 개발자입니다.", "환영합니다.", 3, techSpecs);
 
@@ -64,7 +67,10 @@ public class MemberControllerTest extends RestApiDocumentTest {
     @DisplayName("리뷰어 등록 테스트 - 필드 값이 하나라도 들어있지 않은 경우 실패")
     @Test
     void registerTeacherFailTest() throws Exception {
-        List<TechSpec> techSpecs = Arrays.asList(new TechSpec("java", Arrays.asList("Spring", "Servlet")));
+        List<TechSpec> techSpecs = Arrays.asList(
+                new TechSpec("java", Arrays.asList("spring", "servlet")),
+                new TechSpec("javascript", Arrays.asList("vue", "react"))
+        );
         TeacherRegistrationRequest request =
                 new TeacherRegistrationRequest("백엔드 개발자입니다.", "환영합니다.", null, techSpecs);
 
@@ -75,22 +81,6 @@ public class MemberControllerTest extends RestApiDocumentTest {
                                 .andExpect(status().isBadRequest())
                                 .andDo(print());
     }
-
-//        @DisplayName("리뷰어로 이미 등록되어 있는 사용자가 리뷰어 등록을 할 경우 실패")
-//        @Test
-//        void duplicateTeacherFailTest() throws Exception{
-//            List<TechSpec> techSpecs = Arrays.asList(new TechSpec("java", Arrays.asList("Spring", "Servlet")));
-//            TeacherRegistrationRequest request =
-//                    new TeacherRegistrationRequest("백엔드 개발자입니다.", "환영합니다.", 2, techSpecs);
-//
-//            this.failRestDocsMockMvc.perform(post("/teachers")
-//                    .with(userToken())
-//                    .content(OBJECT_MAPPER.writeValueAsString(request))
-//                    .contentType(MediaType.APPLICATION_JSON))
-//                                    .andExpect(status().isBadRequest())
-//                                    .andDo(print());
-//
-//        }
 
     @DisplayName("리뷰어 목록 조회 테스트 - 성공")
     @Test

7a2c0226fce64d561491b50f9b0f2915b4f98b54
Seed <hsik0225@gmail.com>
2021-07-21 18:35:54 +0900

    fix: FetchJoin 과 Pagination 을 같이 쓰면 생기는 오류 수정 (#109)

--- backend/src/main/java/com/wootech/dropthecode/repository/TeacherFilterRepositoryImpl.java
+++ backend/src/main/java/com/wootech/dropthecode/repository/TeacherFilterRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Objects;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.JPQLQuery;
 import com.wootech.dropthecode.domain.QLanguage;
 import com.wootech.dropthecode.domain.QSkill;
@@ -40,23 +41,32 @@ public class TeacherFilterRepositoryImpl extends QuerydslRepositorySupport imple
         }
         builder.and(teacherProfile.career.goe(career));
 
-        final JPQLQuery<TeacherProfile> query = from(teacherProfile).distinct()
-                                                                    .innerJoin(teacherProfile.languages, teacherLanguage)
-                                                                    .fetchJoin()
-                                                                    .innerJoin(teacherLanguage.language, language)
-                                                                    .fetchJoin()
-                                                                    .innerJoin(teacherProfile.skills, teacherSkill)
-                                                                    .fetchJoin()
-                                                                    .innerJoin(teacherSkill.skill, skill)
-                                                                    .fetchJoin()
-                                                                    .innerJoin(teacherProfile.member)
-                                                                    .fetchJoin()
-                                                                    .where(builder);
+        // 동적 쿼리를 위해 RepositoryImpl 에서 구현
+        // fetch join 과 pagination 을 같이 쓸 때 발생하는 메모리 과부하를 방지하기 위해,
+        // fetch join 쿼리와 pagination 쿼리를 따로 날린다.
+        // 아래는 TeacherProfile 의 Id 만을 구하고, Pagination 을 처리하는 코드
+        final JPQLQuery<Long> query = from(teacherProfile).select(teacherLanguage.id).distinct()
+                                                          .innerJoin(teacherProfile.languages, teacherLanguage)
+                                                          .innerJoin(teacherLanguage.language, language)
+                                                          .innerJoin(teacherProfile.skills, teacherSkill)
+                                                          .innerJoin(teacherSkill.skill, skill)
+                                                          .where(builder);
 
-        final long count = query.fetchCount();
-        final List<TeacherProfile> content = Objects.requireNonNull(getQuerydsl())
-                                                    .applyPagination(pageable, query)
-                                                    .fetch();
-        return new PageImpl<>(content, pageable, count);
+        final QueryResults<Long> teacherProfileIds = Objects.requireNonNull(getQuerydsl())
+                                                            .applyPagination(pageable, query)
+                                                            .fetchResults();
+
+        // InnerJoin 및 FetchJoin 을 사용하기 위해 RepositoryImpl 에서 구현
+        // EntityGraph 는 기본적으로 LeftJoin
+        final List<TeacherProfile> teacherProfiles = from(teacherProfile).distinct()
+                                                                         .innerJoin(teacherProfile.languages, teacherLanguage).fetchJoin()
+                                                                         .innerJoin(teacherLanguage.language, language).fetchJoin()
+                                                                         .innerJoin(teacherProfile.skills, teacherSkill).fetchJoin()
+                                                                         .innerJoin(teacherSkill.skill, skill).fetchJoin()
+                                                                         .innerJoin(teacherProfile.member).fetchJoin()
+                                                                         .where(teacherProfile.id.in(teacherProfileIds.getResults()))
+                                                                         .fetch();
+
+        return new PageImpl<>(teacherProfiles, pageable, teacherProfileIds.getTotal());
     }
 }

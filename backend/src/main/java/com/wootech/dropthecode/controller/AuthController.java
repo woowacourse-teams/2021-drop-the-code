e15662acc0051ee66e5c656939842a8a69a5c47b
kimjunseo <air.junseo@gmail.com>
2021-07-22 14:52:11 +0900

    feat: access token 갱신, 로그아웃 기능 구현

---
+++ backend/src/main/java/com/wootech/dropthecode/controller/AuthController.java
@@ -0,0 +1,55 @@
+package com.wootech.dropthecode.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.wootech.dropthecode.controller.auth.util.AuthorizationExtractor;
+import com.wootech.dropthecode.dto.request.AuthorizationRequest;
+import com.wootech.dropthecode.dto.request.RefreshTokenRequest;
+import com.wootech.dropthecode.dto.response.AccessTokenResponse;
+import com.wootech.dropthecode.dto.response.LoginResponse;
+import com.wootech.dropthecode.service.AuthService;
+import com.wootech.dropthecode.service.OauthService;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthController {
+    private final OauthService oauthService;
+    private final AuthService authService;
+
+    public AuthController(OauthService oauthService, AuthService authService) {
+        this.oauthService = oauthService;
+        this.authService = authService;
+    }
+
+    /**
+     * @title 로그인
+     */
+    @GetMapping("/login/oauth")
+    public ResponseEntity<LoginResponse> login(@ModelAttribute AuthorizationRequest authorizationRequest) {
+        LoginResponse loginResponse = oauthService.login(authorizationRequest);
+        return ResponseEntity.ok().body(loginResponse);
+    }
+
+    /**
+     * @title access token 갱신
+     */
+    @PostMapping(value = "/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<AccessTokenResponse> refreshAccessToken(HttpServletRequest request,
+                                                                  @ModelAttribute RefreshTokenRequest refreshToken) {
+        String accessToken = AuthorizationExtractor.extract(request);
+        return ResponseEntity.ok().body(authService.refreshAccessToken(accessToken, refreshToken));
+    }
+
+    /**
+     * @title 로그아웃
+     */
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        String accessToken = AuthorizationExtractor.extract(request);
+        authService.logout(accessToken);
+        return ResponseEntity.noContent().build();
+    }
+}

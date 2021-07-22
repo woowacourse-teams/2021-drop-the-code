2ab9b7f56308aeaf907116a077c2dbf15111ee45
Jinho <jh8579@gmail.com>
2021-07-22 14:37:43 +0900

    feat: Member 삭제 기능 구현

--- backend/src/main/java/com/wootech/dropthecode/controller/MemberController.java
+++ backend/src/main/java/com/wootech/dropthecode/controller/MemberController.java
@@ -38,6 +38,16 @@ public class MemberController {
     }
 
     /**
+     * @title 유저 삭제
+     */
+    // todo 유저 삭제는 관리자나 본인만 할 수 있도록 허용하는 기능 추가
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<MemberResponse> loginMemberInformation(@PathVariable Long id) {
+        memberService.deleteMember(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
      * @title 리뷰어 등록
      */
     @PostMapping(value = "/teachers")

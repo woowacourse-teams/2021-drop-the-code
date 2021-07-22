0af9c04896da0ad599a861f2bf14659401d88d97
kimjunseo <air.junseo@gmail.com>
2021-07-22 14:54:11 +0900

    test: 테스트 추가 & rest docs 최신화

--- backend/src/test/java/com/wootech/dropthecode/controller/RestApiDocumentTest.java
+++ backend/src/test/java/com/wootech/dropthecode/controller/RestApiDocumentTest.java
@@ -16,6 +16,16 @@ import static capital.scalable.restdocs.misc.AuthorizationSnippet.documentAuthor
 
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public abstract class RestApiDocumentTest {
+    protected static final String CODE = "XfWjrPh7lFumDNFpd3K5";
+    protected static final String GITHUB = "github";
+    protected static final String NAME = "air";
+    protected static final String EMAIL = "air.junseo@gmail.com";
+    protected static final String IMAGE_URL = "https://ssl.pstatic.net/static/pwe/address/img_profile.png";
+    protected static final String STUDENT_ROLE = "STUDENT";
+    protected static final String BEARER = "Bearer";
+    protected static final String ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    protected static final String REFRESH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    protected static final String NEW_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2IiwiaWF0IjoxNjI2OTIyNjkyLCJleHAiOjE2MjY5MjYyOTJ9.dsb5uqMS__VcYToB8QrQFVGOkONeDtMyMv4tMXTUuhY";
 
     @Autowired
     protected ObjectMapper objectMapper;
@@ -28,7 +38,7 @@ public abstract class RestApiDocumentTest {
 
     protected RequestPostProcessor userToken() {
         return request -> {
-            request.addHeader("Authorization", "Bearer aaa.bbb.ccc");
+            request.addHeader("Authorization", BEARER + " " + ACCESS_TOKEN);
             return documentAuthorization(request, "User jwt token required.");
         };
     }

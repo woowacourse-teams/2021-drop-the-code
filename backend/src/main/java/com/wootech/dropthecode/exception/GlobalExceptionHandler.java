89dd1fd0474b35a23e5d9314ab1860f28a9b744c
Seed <hsik0225@gmail.com>
2021-07-23 12:23:04 +0900

    feat: 리퀘스트 파라미터 Validation 적용

--- backend/src/main/java/com/wootech/dropthecode/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/wootech/dropthecode/exception/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package com.wootech.dropthecode.exception;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,15 +23,7 @@ public class GlobalExceptionHandler {
         List<ErrorResponse> errorResponses = new ArrayList<>();
 
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("[");
-            builder.append(fieldError.getField());
-            builder.append("](은)는 ");
-            builder.append(fieldError.getDefaultMessage());
-            builder.append(" 입력된 값은: [");
-            builder.append(fieldError.getRejectedValue());
-            builder.append("] 입니다.");
-            errorResponses.add(new ErrorResponse(builder.toString()));
+            errorResponses.add(new ErrorResponse(polishErrorMessage(e)));
         }
 
         return ResponseEntity.badRequest().body(errorResponses);
@@ -40,8 +35,31 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(Collections.singletonList(new ErrorResponse(name)));
     }
 
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(polishErrorMessage(e)));
+    }
+
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<ErrorResponse> handlePropertyReferenceException(PropertyReferenceException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse("[" + e.getPropertyName() + "](은)는 없는 정렬 조건입니다."));
+    }
+
     @ExceptionHandler(DropTheCodeException.class)
     public ResponseEntity<ErrorResponse> dropTheCodeExceptionHandler(DropTheCodeException e) {
         return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
+    }
+
+    private String polishErrorMessage(BindException e) {
+        FieldError fieldError = Objects.requireNonNull(e.getFieldError());
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        builder.append(fieldError.getField());
+        builder.append("](은)는 ");
+        builder.append(fieldError.getDefaultMessage());
+        builder.append(" 입력된 값은: [");
+        builder.append(fieldError.getRejectedValue());
+        builder.append("] 입니다.");
+        return builder.toString();
     }
 }

b672b73c430cc2be45b72fb2957fdd503bde7a13
Jinho <jh8579@gmail.com>
2021-07-25 19:37:29 +0900

    feat: 테이블 생성과 동시에 데이터 초기화하는 기능 추가

--- backend/src/main/java/com/wootech/dropthecode/domain/Member.java
+++ backend/src/main/java/com/wootech/dropthecode/domain/Member.java
@@ -54,6 +54,14 @@ public class Member extends BaseEntity {
         this.role = role;
     }
 
+    public Member(String email, String name, String imageUrl, Role role) {
+        this.oauthId = oauthId;
+        this.email = email;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.role = role;
+    }
+
     public String getOauthId() {
         return oauthId;
     }

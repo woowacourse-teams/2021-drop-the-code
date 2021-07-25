3bbfe6d4b1b5e65fc571a95bbb1e50e48391c631
Taewan Kim <ktww88@gmail.com>
2021-07-25 21:06:16 +0900

    refactor: 에러 메세지 배열 형태에서 객체로 변경 (#139)
    * refactor: 에러 메세지 배열 형태에서 객체로 변경
    
    * refactor: 에러 메세지 배열 형태에서 객체로 변경

--- backend/src/main/java/com/wootech/dropthecode/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/wootech/dropthecode/exception/GlobalExceptionHandler.java
@@ -1,12 +1,7 @@
 package com.wootech.dropthecode.exception;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,28 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<ErrorResponse>> handleBindingException(MethodArgumentNotValidException e, BindingResult bindingResult) {
-        List<ErrorResponse> errorResponses = new ArrayList<>();
-
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("[");
-            builder.append(fieldError.getField());
-            builder.append("](은)는 ");
-            builder.append(fieldError.getDefaultMessage());
-            builder.append(" 입력된 값은: [");
-            builder.append(fieldError.getRejectedValue());
-            builder.append("] 입니다.");
-            errorResponses.add(new ErrorResponse(builder.toString()));
-        }
-
-        return ResponseEntity.badRequest().body(errorResponses);
+    public ResponseEntity<ErrorResponse> handleBindingException(BindingResult bindingResult) {
+        String message = bindingResult.getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest().body(new ErrorResponse(message));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<List<ErrorResponse>> handleMissingParams(MissingServletRequestParameterException e) {
-        String name = e.getParameterName() + " parameter is missing";
-        return ResponseEntity.badRequest().body(Collections.singletonList(new ErrorResponse(name)));
+    public ResponseEntity<ErrorResponse> handleMissingParams(MissingServletRequestParameterException e) {
+        String message = e.getParameterName() + " parameter is missing";
+        return ResponseEntity.badRequest().body(new ErrorResponse(message));
     }
 
     @ExceptionHandler(DropTheCodeException.class)

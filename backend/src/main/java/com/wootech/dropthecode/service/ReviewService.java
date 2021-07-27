79748f2d0da723d00b775558a6e0d0cd709bfb5b
TaewanKimmmm <ktww88@gmail.com>
2021-07-28 03:01:05 +0900

    refactor: MemberRepository 대신 MemberService를 주입받아 활용하도록 수정

--- backend/src/main/java/com/wootech/dropthecode/service/ReviewService.java
+++ backend/src/main/java/com/wootech/dropthecode/service/ReviewService.java
@@ -1,46 +1,32 @@
 package com.wootech.dropthecode.service;
 
-import java.util.Optional;
-
 import com.wootech.dropthecode.domain.Member;
 import com.wootech.dropthecode.domain.Progress;
 import com.wootech.dropthecode.domain.Review;
 import com.wootech.dropthecode.dto.request.ReviewCreateRequest;
-import com.wootech.dropthecode.exception.DropTheCodeException;
-import com.wootech.dropthecode.repository.MemberRepository;
 import com.wootech.dropthecode.repository.ReviewRepository;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
     private final ReviewRepository reviewRepository;
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public ReviewService(ReviewRepository reviewRepository, MemberRepository memberRepository) {
+    public ReviewService(ReviewRepository reviewRepository, MemberService memberService) {
         this.reviewRepository = reviewRepository;
-        this.memberRepository = memberRepository;
+        this.memberService = memberService;
     }
 
     public Review create(ReviewCreateRequest reviewCreateRequest) {
-        Optional<Member> teacher = memberRepository.findById(reviewCreateRequest.getTeacherId());
-        Optional<Member> student = memberRepository.findById(reviewCreateRequest.getStudentId());
-
-        validateMemberExists(teacher, student);
-
+        Member teacher = memberService.findById(reviewCreateRequest.getTeacherId());
+        Member student = memberService.findById(reviewCreateRequest.getStudentId());
         Review review = new Review
                 (
-                        teacher.get(), student.get(),
+                        teacher, student,
                         reviewCreateRequest.getTitle(), reviewCreateRequest.getContent(),
                         reviewCreateRequest.getPrUrl(), Progress.ON_GOING
                 );
         return reviewRepository.save(review);
-    }
-
-    private void validateMemberExists(Optional<Member> teacher, Optional<Member> student) {
-        if (!teacher.isPresent() || !student.isPresent()) {
-            throw new DropTheCodeException("존재하지 않는 회원이 포함되어 있습니다.", HttpStatus.BAD_REQUEST);
-        }
     }
 }

0bd1ac4f6ab89ef5c735eaedad638d33411f0221
서지환 <seojihwan213@naver.com>
2021-07-29 10:45:40 +0900

    babel preset 추가 및 설정 변경 (#181)
    * chore: babel-minify 추가
    
    Co-authored-by: shinsehantan <shinsehantan@users.noreply.github.com>
    
    * chore: cross-env 설정, babel설정파일을 .js로 수정
    
    Co-authored-by: shinsehantan <shinsehantan@users.noreply.github.com>

---
+++ frontend/babel.config.js
@@ -0,0 +1,26 @@
+/*global process, module*/
+
+const isDevelopment = process.env.NODE_ENV === "development";
+const isProduction = process.env.NODE_ENV === "production";
+
+const presets = [
+  isProduction && "minify",
+  [
+    "@babel/preset-env",
+    {
+      useBuiltIns: "usage",
+      corejs: "3",
+    },
+  ],
+  [
+    "@babel/preset-react",
+    {
+      runtime: "automatic",
+    },
+  ],
+  "@babel/preset-typescript",
+].filter(Boolean);
+
+const plugins = ["babel-plugin-styled-components", isDevelopment && "react-refresh/babel"].filter(Boolean);
+
+module.exports = { presets, plugins };

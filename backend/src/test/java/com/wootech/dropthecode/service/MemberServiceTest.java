6f4aa762c50d246124a78a5eb67455297b1db52a
kimjunseo <air.junseo@gmail.com>
2021-07-30 16:05:50 +0900

    test: 멤버 서비스 테스트 수정

--- backend/src/test/java/com/wootech/dropthecode/service/MemberServiceTest.java
+++ backend/src/test/java/com/wootech/dropthecode/service/MemberServiceTest.java
@@ -49,7 +49,7 @@ class MemberServiceTest {
         // given
         LoginMember loginMember = new LoginMember(1L);
         given(memberRepository.findById(1L))
-                .willReturn(Optional.of(new Member(1L, "oauthId", "air", "air.junseo@gmail.com", "s3://image", Role.STUDENT, null)));
+                .willReturn(Optional.of(new Member(1L, "oauthId", "air.junseo@gmail.com", "air", "s3://image", Role.STUDENT, null)));
 
         // when
         MemberResponse result = memberService.findByLoginMember(loginMember);

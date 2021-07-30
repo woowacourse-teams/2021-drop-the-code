ed2cfd7f4dca1631748dc32c5ba2fc0128350f8b
Seed <hsik0225@gmail.com>
2021-07-30 19:32:34 +0900

    fix: 리뷰어 목록 조회 정렬안되던 오류 수정

--- backend/src/main/java/com/wootech/dropthecode/repository/TeacherFilterRepositoryImpl.java
+++ backend/src/main/java/com/wootech/dropthecode/repository/TeacherFilterRepositoryImpl.java
@@ -1,11 +1,12 @@
 package com.wootech.dropthecode.repository;
 
 import java.util.List;
-import java.util.Objects;
 
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.QueryResults;
-import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.wootech.dropthecode.domain.Language;
 import com.wootech.dropthecode.domain.Skill;
 import com.wootech.dropthecode.domain.TeacherProfile;
@@ -14,6 +15,7 @@ import com.wootech.dropthecode.repository.support.Querydsl4RepositorySupport;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import static com.wootech.dropthecode.domain.QLanguage.language;
@@ -32,24 +34,32 @@ public class TeacherFilterRepositoryImpl extends Querydsl4RepositorySupport impl
 
     @Override
     public Page<TeacherProfile> findAll(List<Language> languages, List<Skill> skills, int career, Pageable pageable) {
-        final QueryResults<TeacherProfile> queryResults = findTeacherProfileIdsByPageable(languages, skills, career, pageable);
-        final List<TeacherProfile> teacherProfiles = findAllByIds(queryResults);
-        return new PageImpl<>(teacherProfiles, pageable, queryResults.getTotal());
+        final Page<TeacherProfile> queryResults = findTeacherProfileIdsByPageable(languages, skills, career, pageable);
+        final List<TeacherProfile> teacherProfiles = findAllByIds(queryResults, pageable);
+        return new PageImpl<>(teacherProfiles, pageable, queryResults.getTotalPages());
     }
 
-    private List<TeacherProfile> findAllByIds(QueryResults<TeacherProfile> teacherProfiles) {
-        return getQueryFactory().select(teacherProfile)
-                                .from(teacherProfile).distinct()
-                                .innerJoin(teacherProfile.languages, teacherLanguage).fetchJoin()
-                                .innerJoin(teacherLanguage.language, language).fetchJoin()
-                                .innerJoin(teacherProfile.skills, teacherSkill).fetchJoin()
-                                .innerJoin(teacherSkill.skill, skill).fetchJoin()
-                                .innerJoin(teacherProfile.member).fetchJoin()
-                                .where(teacherProfile.in(teacherProfiles.getResults()))
-                                .fetch();
+    private List<TeacherProfile> findAllByIds(Page<TeacherProfile> teacherProfiles, Pageable pageable) {
+        final JPAQuery<TeacherProfile> query = getQueryFactory().select(teacherProfile)
+                                                                .from(teacherProfile).distinct()
+                                                                .innerJoin(teacherProfile.languages, teacherLanguage).fetchJoin()
+                                                                .innerJoin(teacherLanguage.language, language).fetchJoin()
+                                                                .innerJoin(teacherProfile.skills, teacherSkill).fetchJoin()
+                                                                .innerJoin(teacherSkill.skill, skill).fetchJoin()
+                                                                .innerJoin(teacherProfile.member).fetchJoin()
+                                                                .where(teacherProfile.in(teacherProfiles.getContent()));
+
+        for (Sort.Order o : pageable.getSort()) {
+            PathBuilder<Object> orderByExpression = new PathBuilder<>(Object.class, "teacherProfile");
+
+            query.orderBy(new OrderSpecifier(o.isAscending() ? Order.ASC : Order.DESC, orderByExpression.get(o.getProperty())));
+        }
+
+        return query.fetch();
     }
 
-    private QueryResults<TeacherProfile> findTeacherProfileIdsByPageable(List<Language> languages, List<Skill> skills, int career, Pageable pageable) {
+
+    private Page<TeacherProfile> findTeacherProfileIdsByPageable(List<Language> languages, List<Skill> skills, int career, Pageable pageable) {
         BooleanBuilder builder = new BooleanBuilder();
         builder.and(language.in(languages));
         if (!skills.isEmpty()) {
@@ -57,16 +67,16 @@ public class TeacherFilterRepositoryImpl extends Querydsl4RepositorySupport impl
         }
         builder.and(teacherProfile.career.goe(career));
 
-        final JPQLQuery<TeacherProfile> query = getQueryFactory().select(teacherProfile).distinct()
-                                                                 .from(teacherProfile)
-                                                                 .innerJoin(teacherProfile.languages, teacherLanguage)
-                                                                 .innerJoin(teacherLanguage.language, language)
-                                                                 .innerJoin(teacherProfile.skills, teacherSkill)
-                                                                 .innerJoin(teacherSkill.skill, skill)
-                                                                 .where(builder);
+        return applyPagination(pageable, contentQuery -> query(builder));
+    }
 
-        return Objects.requireNonNull(getQuerydsl())
-                      .applyPagination(pageable, query)
-                      .fetchResults();
+    private JPAQuery<TeacherProfile> query(BooleanBuilder builder) {
+        return getQueryFactory().select(teacherProfile).distinct()
+                                .from(teacherProfile)
+                                .innerJoin(teacherProfile.languages, teacherLanguage)
+                                .innerJoin(teacherLanguage.language, language)
+                                .innerJoin(teacherProfile.skills, teacherSkill)
+                                .innerJoin(teacherSkill.skill, skill)
+                                .where(builder);
     }
 }

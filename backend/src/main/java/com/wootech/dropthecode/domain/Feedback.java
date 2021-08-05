a8448d391284fe722e08eda2057ba4ad9b9cac4c
TaewanKimmmm <ktww88@gmail.com>
2021-08-05 21:21:04 +0900

    refactor: Feedback 엔티티 lombok 적용

--- backend/src/main/java/com/wootech/dropthecode/domain/Feedback.java
+++ backend/src/main/java/com/wootech/dropthecode/domain/Feedback.java
@@ -2,6 +2,13 @@ package com.wootech.dropthecode.domain;
 
 import javax.persistence.*;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Feedback {
     @Id
@@ -19,28 +26,10 @@ public class Feedback {
     @JoinColumn(name = "id", unique = true, foreignKey = @ForeignKey(name = "fk_feedback_to_review"))
     private Review review;
 
-    protected Feedback() {
-    }
-
+    @Builder
     public Feedback(Review review, Integer star, String comment) {
         this.review = review;
         this.star = star;
         this.comment = comment;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Integer getStar() {
-        return star;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public Review getReview() {
-        return review;
     }
 }

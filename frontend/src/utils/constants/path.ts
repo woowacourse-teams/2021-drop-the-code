44997eb0e868c789137979f81d72e19e9c1a6fa9
shinsehantan <shinsehantan@users.noreply.github.com>
2021-08-05 11:06:47 +0900

    refactor: HISTORY 키값 MY_PAGE로 변경

--- frontend/src/utils/constants/path.ts
+++ frontend/src/utils/constants/path.ts
@@ -3,7 +3,7 @@ export const PATH = {
   REVIEWER_DETAIL: "/reviewer/:reviewerId",
   REVIEWER_REGISTER: "/reviewer/register",
   REVIEW_DETAIL: "/review/:reviewId",
-  HISTORY: "/myPage",
+  MY_PAGE: "/myPage",
   REVIEW_REQUEST: "/review/request/:reviewerId",
   REDIRECT_OAUTH: "/redirect/oauth",
   NOT_FOUND: "*",
--- frontend/src/utils/constants/route.ts
+++ frontend/src/utils/constants/route.ts
@@ -12,7 +12,7 @@ export const ROUTE: RouteShape[] = [
   { path: PATH.MAIN, Component: Main, isPrivate: false, exact: true },
   { path: PATH.REVIEWER_REGISTER, Component: ReviewerRegister, isPrivate: true, exact: true },
   { path: PATH.REVIEWER_DETAIL, Component: ReviewerDetail, isPrivate: false, exact: true },
-  { path: PATH.HISTORY, Component: MyPage, isPrivate: true, exact: true },
+  { path: PATH.MY_PAGE, Component: MyPage, isPrivate: true, exact: true },
   { path: PATH.REVIEW_DETAIL, Component: ReviewDetail, isPrivate: false, exact: true },
   { path: PATH.REDIRECT_OAUTH, Component: RedirectOAuth, isPrivate: false, exact: true },
 ];
@@ -21,4 +21,4 @@ export const ROLE_MENU: RoleNavMenuShape[] = [
   { to: PATH.REVIEWER_REGISTER, children: "리뷰어 등록하기", isTeacher: false },
 ];
 
-export const NAV_MENU: NavMenuShape[] = [{ to: PATH.HISTORY, children: "마이페이지", isPrivate: true }];
+export const NAV_MENU: NavMenuShape[] = [{ to: PATH.MY_PAGE, children: "마이페이지", isPrivate: true }];

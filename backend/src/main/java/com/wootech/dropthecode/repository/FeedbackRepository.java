f792bb72c19928b62db3e418351f6389f5b3395a
TaewanKimmmm <ktww88@gmail.com>
2021-08-05 21:21:19 +0900

    refactor: 불필요한 @Repository 어노테이션 삭제

--- backend/src/main/java/com/wootech/dropthecode/repository/FeedbackRepository.java
+++ backend/src/main/java/com/wootech/dropthecode/repository/FeedbackRepository.java
@@ -5,7 +5,6 @@ import com.wootech.dropthecode.domain.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 
 }

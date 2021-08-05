02c59e1c9d882a542de2d6f14fa4677ced2fd83e
seojihwan <seojihwan213@naver.com>
2021-08-05 18:36:34 +0900

    test: describe로 비슷한 유형의 케이스 묶어주기

--- frontend/src/pages/MyPage/MyPage.test.tsx
+++ frontend/src/pages/MyPage/MyPage.test.tsx
@@ -14,69 +14,74 @@ import { PATH } from "utils/constants/path";
 const { findByText, findByRole } = screen;
 
 describe("마이페이지 테스트", () => {
-  it("사용자가 로그인 하지 않은 경우 메인페이지로 리다이렉션 된다.", async () => {
-    mockingAnonymousAuth();
+  describe("로그인 하지 않은경우", () => {
+    it("페이지 진입시 메인페이지로 리다이렉션 된다.", async () => {
+      mockingAnonymousAuth();
 
-    render(
-      <MemoryRouter initialEntries={["/mypage"]}>
-        <App />
-      </MemoryRouter>
-    );
+      render(
+        <MemoryRouter initialEntries={["/mypage"]}>
+          <App />
+        </MemoryRouter>
+      );
 
-    const title = await findByText("리뷰어 찾기");
-    expect(title).toBeVisible();
+      const title = await findByText("리뷰어 찾기");
+      expect(title).toBeVisible();
+    });
   });
 
-  it("사용자가 로그인 되어 있는 경우 자신의 이름과 정보를 확인할 수 있다.", async () => {
-    mockingToken();
-    mockingStudentAuth();
+  describe("사용자가 로그인 되어 있는 경우", () => {
+    beforeEach(() => {
+      mockingToken();
+      mockingStudentAuth();
+    });
 
-    render(<MyPage />);
+    it("자신의 이름, 깃허브 링크가 화면에 나타난다.", async () => {
+      render(<MyPage />);
 
-    const name = await findByText(studentAuth.name);
-    const githubUrl = await findByText(studentAuth.githubUrl);
+      const name = await findByText(studentAuth.name);
+      const githubUrl = await findByText(studentAuth.githubUrl);
 
-    expect(name).toBeVisible();
-    expect(githubUrl).toBeVisible();
-  });
+      expect(name).toBeVisible();
+      expect(githubUrl).toBeVisible();
+    });
 
-  it("사용자가 로그인 되어 있는 경우 회원 탈퇴하면 메인 페이지로 이동한다", async () => {
-    mockingToken();
-    mockingStudentAuth();
+    it("회원 탈퇴하면 메인 페이지로 이동한다", async () => {
+      render(
+        <MemoryRouter initialEntries={["/mypage"]}>
+          <Switch>
+            <Route path={PATH.MAIN} exact>
+              <Main />
+            </Route>
+            <Route path={PATH.MY_PAGE} exact>
+              <MyPage />
+            </Route>
+          </Switch>
+        </MemoryRouter>
+      );
 
-    render(
-      <MemoryRouter initialEntries={["/mypage"]}>
-        <Switch>
-          <Route path={PATH.MAIN} exact>
-            <Main />
-          </Route>
-          <Route path={PATH.MY_PAGE} exact>
-            <MyPage />
-          </Route>
-        </Switch>
-      </MemoryRouter>
-    );
+      const leaveButton = await findByRole("button", { name: "회원 탈퇴하기" });
+      fireEvent.click(leaveButton);
 
-    const leaveButton = await findByRole("button", { name: "회원 탈퇴하기" });
-    fireEvent.click(leaveButton);
+      const confirmButton = await findByRole("button", { name: "확인" });
+      fireEvent.click(confirmButton);
 
-    const confirmButton = await findByRole("button", { name: "확인" });
-    fireEvent.click(confirmButton);
+      const toast = await findByText(SUCCESS_MESSAGE.API.AUTH.DELETE);
+      expect(toast).toBeInTheDocument();
 
-    const toast = await findByText(SUCCESS_MESSAGE.API.AUTH.DELETE);
-    expect(toast).toBeInTheDocument();
-
-    const title = await findByText("리뷰어 찾기");
-    expect(title).toBeVisible();
+      const title = await findByText("리뷰어 찾기");
+      expect(title).toBeVisible();
+    });
   });
 
   describe("사용자가 리뷰어인 경우", () => {
-    it("리뷰어 정보가 화면에 나타난다.", async () => {
+    beforeEach(() => {
       mockingToken();
       mockingTeacherAuth();
 
       render(<MyPage />);
+    });
 
+    it("리뷰어 정보가 화면에 나타난다.", async () => {
       const title = await findByText(new RegExp(reviewers[0].title));
       const career = await findByText(new RegExp(String(reviewers[0].career + "년")));
 
@@ -85,11 +90,6 @@ describe("마이페이지 테스트", () => {
     });
 
     it("리뷰어 정보를 삭제 할 수 있다.", async () => {
-      mockingToken();
-      mockingTeacherAuth();
-
-      render(<MyPage />);
-
       const deleteButton = await findByRole("button", { name: "삭제" });
       fireEvent.click(deleteButton);
 
@@ -101,11 +101,6 @@ describe("마이페이지 테스트", () => {
     });
 
     it("리뷰어 정보를 수정 할 수 있다.", async () => {
-      mockingToken();
-      mockingTeacherAuth();
-
-      render(<MyPage />);
-
       const editButton = await findByRole("button", { name: "수정" });
       fireEvent.click(editButton);
 

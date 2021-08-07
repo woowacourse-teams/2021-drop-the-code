a58d62c7adc39059016020d7a4eb042f1cab6051
Jinho <jh8579@gmail.com>
2021-08-08 05:00:21 +0900

    refactor: RestController로 변경

--- backend/src/main/java/com/wootech/dropthecode/controller/FeedbackController.java
+++ backend/src/main/java/com/wootech/dropthecode/controller/FeedbackController.java
@@ -6,12 +6,12 @@ import com.wootech.dropthecode.service.FeedbackService;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/feedbacks")
 public class FeedbackController {
 

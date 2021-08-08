aa6e659e1fa49c5467b604144071db55eddf664b
kimjunseo <air.junseo@gmail.com>
2021-08-09 03:29:44 +0900

    test: 리뷰 수정 인수 테스트

--- backend/src/test/java/com/wootech/dropthecode/acceptance/ReviewAcceptanceTest.java
+++ backend/src/test/java/com/wootech/dropthecode/acceptance/ReviewAcceptanceTest.java
@@ -434,8 +434,7 @@ class ReviewAcceptanceTest {
     @Nested
     @DisplayName("리뷰 목록 조회")
     class FindReview extends AcceptanceTest {
-        private String reviewId1;
-        private String reviewId2;
+        private String reviewId;
 
         @Override
         @BeforeEach
@@ -452,22 +451,14 @@ class ReviewAcceptanceTest {
                                                         .content("초보라 맞게 한지 잘 모르겠네요.. 잘 부탁드려요!")
                                                         .prUrl("https://github.com/woowacourse-teams/2021-drop-the-code/pull/262")
                                                         .build();
-            ReviewRequest reviewRequest2 = ReviewRequest.builder()
-                                                        .studentId(student.getId())
-                                                        .teacherId(teacher.getId())
-                                                        .title("코리부!")
-                                                        .content("기대중입니다!")
-                                                        .prUrl("https://github.com/woowacourse-teams/2021-drop-the-code/pull/262")
-                                                        .build();
-            reviewId1 = 새로운_리뷰_요청(student.getAccessToken(), reviewRequest1).header("Location").substring(9);
-            reviewId2 = 새로운_리뷰_요청(student.getAccessToken(), reviewRequest2).header("Location").substring(9);
+            reviewId = 새로운_리뷰_요청(student.getAccessToken(), reviewRequest1).header("Location").substring(9);
         }
 
         @Test
         @DisplayName("리뷰 상세 목록 조회 성공")
         void findReviewByIdSuccess() {
             // when
-            ExtractableResponse<Response> response = 리뷰_상세_조회_요청(reviewId1);
+            ExtractableResponse<Response> response = 리뷰_상세_조회_요청(reviewId);
             ReviewResponse result = response.as(ReviewResponse.class);
 
             // then
@@ -497,6 +488,117 @@ class ReviewAcceptanceTest {
 
             // then
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThat(error.getErrorMessage()).isNotNull();
+        }
+    }
+
+    @Nested
+    @DisplayName("리뷰 수정")
+    class UpdateReview extends AcceptanceTest {
+        private LoginResponse student;
+        private LoginResponse teacher;
+        private String reviewId;
+
+        @Override
+        @BeforeEach
+        public void setUp() {
+            super.setUp();
+            // given
+            student = 로그인되어_있음("air");
+            teacher = 로그인되어_있음("curry");
+
+            ReviewRequest reviewRequest = ReviewRequest.builder()
+                                                       .studentId(student.getId())
+                                                       .teacherId(teacher.getId())
+                                                       .title("리뷰 요청합니다!")
+                                                       .content("초보라 맞게 한지 잘 모르겠네요.. 잘 부탁드려요!")
+                                                       .prUrl("https://github.com/woowacourse-teams/2021-drop-the-code/pull/262")
+                                                       .build();
+            reviewId = 새로운_리뷰_요청(student.getAccessToken(), reviewRequest).header("Location").substring(9);
+        }
+
+        @Test
+        @DisplayName("리뷰 수정 성공")
+        void updateReviewSuccess() {
+            // given
+            ReviewRequest reviewRequest = ReviewRequest.builder()
+                                                       .studentId(student.getId())
+                                                       .teacherId(teacher.getId())
+                                                       .title("[수정] 리뷰 요청합니다!")
+                                                       .content("[수정] 초보라 맞게 한지 잘 모르겠네요.. 잘 부탁드려요!")
+                                                       .prUrl("[수정] https://github.com/woowacourse-teams/2021-drop-the-code/pull/262")
+                                                       .build();
+
+            // when
+            ExtractableResponse<Response> response = 리뷰_수정_요청(reviewId, student.getAccessToken(), reviewRequest);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 리뷰인 경우")
+        void notExistReview() {
+            // given
+            String notExistReviewId = "100";
+            ReviewRequest reviewRequest = ReviewRequest.builder()
+                                                       .studentId(student.getId())
+                                                       .teacherId(teacher.getId())
+                                                       .title("[수정] 리뷰 요청합니다!")
+                                                       .content("[수정] 초보라 맞게 한지 잘 모르겠네요.. 잘 부탁드려요!")
+                                                       .prUrl("[수정] https://github.com/woowacourse-teams/2021-drop-the-code/pull/262")
+                                                       .build();
+
+            // when
+            ExtractableResponse<Response> response = 리뷰_수정_요청(notExistReviewId, student.getAccessToken(), reviewRequest);
+            ErrorResponse error = 예외_결과(response);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThat(error.getErrorMessage()).isNotNull();
+        }
+
+
+        @Test
+        @DisplayName("로그인 한 멤버와 리뷰의 student가 같지 않은 경우")
+        void noAuthorization() {
+            // given
+            LoginResponse otherStudent = 로그인되어_있음("allie");
+            ReviewRequest reviewRequest = ReviewRequest.builder()
+                                                       .studentId(student.getId())
+                                                       .teacherId(teacher.getId())
+                                                       .title("[수정] 리뷰 요청합니다!")
+                                                       .content("[수정] 초보라 맞게 한지 잘 모르겠네요.. 잘 부탁드려요!")
+                                                       .prUrl("[수정] https://github.com/woowacourse-teams/2021-drop-the-code/pull/262")
+                                                       .build();
+
+            // when
+            ExtractableResponse<Response> response = 리뷰_수정_요청(reviewId, otherStudent.getAccessToken(), reviewRequest);
+            ErrorResponse error = 예외_결과(response);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+            assertThat(error.getErrorMessage()).isNotNull();
+        }
+
+        @Test
+        @DisplayName("유효하지 않은 access token")
+        void invalidAccessToken() {
+            // given
+            ReviewRequest reviewRequest = ReviewRequest.builder()
+                                                       .studentId(student.getId())
+                                                       .teacherId(teacher.getId())
+                                                       .title("[수정] 리뷰 요청합니다!")
+                                                       .content("[수정] 초보라 맞게 한지 잘 모르겠네요.. 잘 부탁드려요!")
+                                                       .prUrl("[수정] https://github.com/woowacourse-teams/2021-drop-the-code/pull/262")
+                                                       .build();
+
+            // when
+            ExtractableResponse<Response> response = 새로운_리뷰_요청("invalid.access.token", reviewRequest);
+            ErrorResponse error = 예외_결과(response);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
             assertThat(error.getErrorMessage()).isNotNull();
         }
     }
@@ -626,6 +728,19 @@ class ReviewAcceptanceTest {
                           .log().all()
                           .when()
                           .get("/reviews/{id}", id)
+                          .then()
+                          .log().all()
+                          .extract();
+    }
+
+    public static ExtractableResponse<Response> 리뷰_수정_요청(String id, String accessToken, ReviewRequest request) {
+        return RestAssured.given()
+                          .log().all()
+                          .header("Authorization", "Bearer " + accessToken)
+                          .contentType(APPLICATION_JSON_VALUE)
+                          .body(request)
+                          .when()
+                          .patch("/reviews/{id}", id)
                           .then()
                           .log().all()
                           .extract();

d7ccbf91b6b6981b078f3e2cdb24dc0bde70b72f
seojihwan <seojihwan213@naver.com>
2021-08-09 21:14:57 +0900

    refactor: development에 대한 분기 추가

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,6 +4,7 @@ const NODE_ENV = process.env.NODE_ENV;
 
 if (!NODE_ENV) throw Error("NODE_ENV environment variable is not defined");
 
+const isDevelopment = NODE_ENV === "development";
 const isProduction = NODE_ENV === "production";
 
 const presets = [
@@ -29,6 +30,6 @@ const presets = [
   "@babel/preset-typescript",
 ].filter(Boolean);
 
-const plugins = ["babel-plugin-styled-components", !isProduction && "react-refresh/babel"].filter(Boolean);
+const plugins = ["babel-plugin-styled-components", isDevelopment && "react-refresh/babel"].filter(Boolean);
 
 module.exports = { presets, plugins };

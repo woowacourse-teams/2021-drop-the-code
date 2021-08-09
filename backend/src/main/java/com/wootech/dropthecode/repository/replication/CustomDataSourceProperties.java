9e01102e34059387450e0ddea57614e4d9f897ff
kimjunseo <air.junseo@gmail.com>
2021-08-09 17:53:44 +0900

    refactor: setter 추가

--- backend/src/main/java/com/wootech/dropthecode/repository/replication/CustomDataSourceProperties.java
+++ backend/src/main/java/com/wootech/dropthecode/repository/replication/CustomDataSourceProperties.java
@@ -16,12 +16,24 @@ public class CustomDataSourceProperties {
         return url;
     }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Map<String, Slave> getSlave() {

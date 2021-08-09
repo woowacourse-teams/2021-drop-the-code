d1a47c347e2ac2a109363a99491aa189fd0dc9fc
kimjunseo <air.junseo@gmail.com>
2021-08-09 17:00:15 +0900

    refactor: prod 환경 auto datasource 제거

--- backend/src/main/java/com/wootech/dropthecode/config/CustomDataSourceConfig.java
+++ backend/src/main/java/com/wootech/dropthecode/config/CustomDataSourceConfig.java
@@ -2,10 +2,15 @@ package com.wootech.dropthecode.config;
 
 import com.wootech.dropthecode.domain.CustomDataSourceProperties;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("prod")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties(CustomDataSourceProperties.class)
 public class CustomDataSourceConfig {
 }

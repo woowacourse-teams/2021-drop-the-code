4d65781bdc90548f7b7d533d781571c26cc39543
Jinho <jh8579@gmail.com>
2021-08-10 01:17:58 +0900

    refactor: 중복 메소드 통합

--- backend/src/test/java/com/wootech/dropthecode/acceptance/MemberAcceptanceTest.java
+++ backend/src/test/java/com/wootech/dropthecode/acceptance/MemberAcceptanceTest.java
@@ -51,9 +51,6 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @Autowired
     LanguageSkillRepository languageSkillRepository;
 
-    @MockBean
-    private InMemoryProviderRepository inMemoryProviderRepository;
-
     @BeforeEach
     void initializeData() {
         Map<Long, Language> languageMap = insertLanguage()
@@ -106,7 +103,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("로그인 한 유저 정보 조회 성공")
     void membersMeSuccess() {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         // when
         ExtractableResponse<Response> response = 로그인_한_유저_정보_조회_요청(loginResponse);
@@ -147,7 +144,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("멤버 삭제 성공")
     void deleteMemberSuccess() {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         // when
         ExtractableResponse<Response> response = 멤버_삭제_요청(loginResponse);
@@ -184,7 +181,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 성공")
     void registerTeacherSuccess() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TechSpec techSpecJava = 정삭적인_리뷰어_언어_기술_스택_Java();
         TechSpec techSpecJavascript = 정삭적인_리뷰어_언어_기술_스택_Javascript();
@@ -214,7 +211,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 선생님 기술 스펙에 존재하지 않는 언어가 들어있는 경우")
     void notExistLanguageInTeacherTechSpec() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TechSpec techSpecNotExistLanguage = 유효하지_않은_스택_존재하지_않는_언어();
         TeacherRegistrationRequest request = 정삭적인_리뷰어_정보(techSpecNotExistLanguage);
@@ -230,7 +227,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 선생님 기술 스펙에 존재하지 않는 기술이 들어있는 경우")
     void notExistSkillInTeacherTechSpec() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TechSpec techSpecNotExistSkill = 유효하지_않은_스택_존재하지_않은_기술();
         TeacherRegistrationRequest request = 정삭적인_리뷰어_정보(techSpecNotExistSkill);
@@ -246,7 +243,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 선생님 기술이 언어에 포함되지 않는 경우")
     void notContainSkill() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TechSpec techSpecSkillsNotInLanguage = 유효하지_않은_스택_기술이_언어에_속해있지_않은_스택();
         TeacherRegistrationRequest request = 정삭적인_리뷰어_정보(techSpecSkillsNotInLanguage);
@@ -262,7 +259,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 자기소개 제목이 빈 경우")
     void emptyTitle() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TechSpec techSpecJava = 정삭적인_리뷰어_언어_기술_스택_Java();
         TeacherRegistrationRequest request = 유효하지_않은_리뷰어_정보_제목_없음(techSpecJava);
@@ -278,7 +275,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 자기소개 내용이 빈 경우")
     void emptyContent() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TechSpec techSpecJava = 정삭적인_리뷰어_언어_기술_스택_Java();
         TeacherRegistrationRequest request = 유효하지_않은_리뷰어_정보_내용_없음(techSpecJava);
@@ -294,7 +291,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 경력이 빈 경우")
     void emptyCareer() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TechSpec techSpecJava = 정삭적인_리뷰어_언어_기술_스택_Java();
         TeacherRegistrationRequest request = 유효하지_않은_리뷰어_정보_경력_없음(techSpecJava);
@@ -310,7 +307,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 선생님 기술 스펙이 빈 경우")
     void emptyTechSpec() throws Exception {
         // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
+        LoginResponse loginResponse = 학생_로그인되어_있음("air");
 
         TeacherRegistrationRequest request = 유효하지_않은_리뷰어_정보_기술_스택_없음();
 
@@ -325,7 +322,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 등록 실패 - 이미 선생님으로 등록된 경우")
     void alreadyRegisterTeacher() throws Exception {
         // given
-        LoginResponse loginResponse = 리뷰어_로그인되어_있음();
+        LoginResponse loginResponse = 리뷰어_로그인되어_있음("air");
 
         TechSpec techSpecJava = 정삭적인_리뷰어_언어_기술_스택_Java();
         TechSpec techSpecJavascript = 정삭적인_리뷰어_언어_기술_스택_Javascript();
@@ -396,7 +393,7 @@ public class MemberAcceptanceTest extends AcceptanceTest{
     @DisplayName("선생님 단일 조회 성공")
     void findTeacherByIdSuccess() throws Exception {
         // given
-        LoginResponse loginResponse = 리뷰어_로그인되어_있음();
+        LoginResponse loginResponse = 리뷰어_로그인되어_있음("air");
         // when
         ExtractableResponse<Response> response = RestAssured.given()
                                                             .log().all()
@@ -429,67 +426,6 @@ public class MemberAcceptanceTest extends AcceptanceTest{
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
-    public LoginResponse 학생_로그인되어_있음() {
-        ExtractableResponse<Response> response = 로그인_요청();
-        return response.as(LoginResponse.class);
-    }
-
-    public LoginResponse 리뷰어_로그인되어_있음() throws Exception {
-        선생님_등록_요청();
-        ExtractableResponse<Response> response = 로그인_요청();
-        return response.as(LoginResponse.class);
-    }
-
-    public LoginResponse 유효하지_않은_로그인() {
-        return LoginResponse.builder()
-                            .accessToken("INVALID_TOKEN")
-                            .build();
-    }
-
-    public ExtractableResponse<Response> 로그인_요청() {
-        String serverAddress = "http://localhost:" + port;
-        given(inMemoryProviderRepository.findByProviderName("github"))
-                .willReturn(OauthProvider.builder()
-                                         .clientId("fakeClientId")
-                                         .clientSecret("fakeClientSecret")
-                                         .tokenUrl(serverAddress + "/fake/login/oauth/access_token")
-                                         .userInfoUrl(serverAddress + "/fake/user")
-                                         .build());
-
-        return RestAssured.given()
-                          .log().all()
-                          .when()
-                          .get("/login/oauth?providerName=github&code=authorizationCode")
-                          .then()
-                          .log().all()
-                          .statusCode(HttpStatus.OK.value())
-                          .extract();
-    }
-
-    public void 선생님_등록_요청() throws Exception {
-        // given
-        LoginResponse loginResponse = 학생_로그인되어_있음();
-
-        TechSpec techSpecJava = 정삭적인_리뷰어_언어_기술_스택_Java();
-        TechSpec techSpecJavascript = 정삭적인_리뷰어_언어_기술_스택_Javascript();
-        TeacherRegistrationRequest request = 정삭적인_리뷰어_정보(techSpecJava, techSpecJavascript);
-
-        // when
-        ExtractableResponse<Response> response = 선생님_등록_요청(loginResponse, request);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    private TeacherRegistrationRequest 정삭적인_리뷰어_정보(TechSpec... techSpec) {
-        return TeacherRegistrationRequest.builder()
-                                         .title("네이버 백엔드 개발자")
-                                         .content("열심히 리뷰하겠습니다!")
-                                         .career(3)
-                                         .techSpecs(Arrays.asList(techSpec))
-                                         .build();
-    }
-
     private TeacherRegistrationRequest 유효하지_않은_리뷰어_정보_제목_없음(TechSpec... techSpec) {
         return TeacherRegistrationRequest.builder()
                                          .content("열심히 리뷰하겠습니다!")
@@ -520,14 +456,6 @@ public class MemberAcceptanceTest extends AcceptanceTest{
                                          .content("열심히 리뷰하겠습니다!")
                                          .career(3)
                                          .build();
-    }
-
-    private TechSpec 정삭적인_리뷰어_언어_기술_스택_Java() {
-        return TechSpec.builder().language("java").skills(Collections.singletonList("spring")).build();
-    }
-
-    private TechSpec 정삭적인_리뷰어_언어_기술_스택_Javascript() {
-        return TechSpec.builder().language("javascript").skills(Arrays.asList("react", "vue")).build();
     }
 
     private TechSpec 유효하지_않은_스택_기술이_언어에_속해있지_않은_스택() {

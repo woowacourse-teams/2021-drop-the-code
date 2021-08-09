61dd49bc4f65f4591c1a7e4262c94cdb01ae49e1
seojihwan <seojihwan213@naver.com>
2021-08-09 20:51:40 +0900

    refactor: babel config 수정
    - NODE_ENV환경변수 미 설정에 대한 에러 처리
    - NODE_ENV를 production과 같은지로 분기

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,10 @@
 /*global process, module*/
 
-const isDevelopment = process.env.NODE_ENV === "development";
-const isProduction = process.env.NODE_ENV === "production";
+const NODE_ENV = process.env.NODE_ENV;
+
+if (!NODE_ENV) throw Error("NODE_ENV environment variable is not defined");
+
+const isProduction = NODE_ENV === "production";
 
 const presets = [
   isProduction && [
@@ -26,6 +29,6 @@ const presets = [
   "@babel/preset-typescript",
 ].filter(Boolean);
 
-const plugins = ["babel-plugin-styled-components", isDevelopment && "react-refresh/babel"].filter(Boolean);
+const plugins = ["babel-plugin-styled-components", !isProduction && "react-refresh/babel"].filter(Boolean);
 
 module.exports = { presets, plugins };

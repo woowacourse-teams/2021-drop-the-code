d31f38633afa6a22e236eff7d4203e796dd74876
kimjunseo <air.junseo@gmail.com>
2021-08-10 03:58:56 +0900

    refactor: OauthProperties 롬복 적용

--- backend/src/main/java/com/wootech/dropthecode/domain/oauth/OauthProperties.java
+++ backend/src/main/java/com/wootech/dropthecode/domain/oauth/OauthProperties.java
@@ -4,78 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 @ConfigurationProperties(prefix = "oauth2")
 public class OauthProperties {
     private final Map<String, User> user = new HashMap<>();
 
     private final Map<String, Provider> provider = new HashMap<>();
 
-    public Map<String, User> getUser() {
-        return user;
-    }
-
-    public Map<String, Provider> getProvider() {
-        return provider;
-    }
-
+    @Getter
+    @Setter
     public static class User {
         private String clientId;
         private String clientSecret;
         private String redirectUri;
-
-        public String getClientId() {
-            return clientId;
-        }
-
-        public void setClientId(String clientId) {
-            this.clientId = clientId;
-        }
-
-        public String getClientSecret() {
-            return clientSecret;
-        }
-
-        public void setClientSecret(String clientSecret) {
-            this.clientSecret = clientSecret;
-        }
-
-        public String getRedirectUri() {
-            return redirectUri;
-        }
-
-        public void setRedirectUri(String redirectUri) {
-            this.redirectUri = redirectUri;
-        }
     }
 
+    @Getter
+    @Setter
     public static class Provider {
         private String tokenUri;
         private String userInfoUri;
         private String userNameAttribute;
-
-        public String getTokenUri() {
-            return tokenUri;
-        }
-
-        public void setTokenUri(String tokenUri) {
-            this.tokenUri = tokenUri;
-        }
-
-        public String getUserInfoUri() {
-            return userInfoUri;
-        }
-
-        public void setUserInfoUri(String userInfoUri) {
-            this.userInfoUri = userInfoUri;
-        }
-
-        public String getUserNameAttribute() {
-            return userNameAttribute;
-        }
-
-        public void setUserNameAttribute(String userNameAttribute) {
-            this.userNameAttribute = userNameAttribute;
-        }
     }
 }

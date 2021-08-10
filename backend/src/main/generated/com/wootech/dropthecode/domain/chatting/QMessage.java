b96a7f6e6e860eadf9b128e9bd9d5637241a2d07
TaewanKimmmm <ktww88@gmail.com>
2021-08-11 05:33:05 +0900

    fix: mappedBy 주인 수정

---
+++ backend/src/main/generated/com/wootech/dropthecode/domain/chatting/QMessage.java
@@ -0,0 +1,62 @@
+package com.wootech.dropthecode.domain.chatting;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMessage is a Querydsl query type for Message
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QMessage extends EntityPathBase<Message> {
+
+    private static final long serialVersionUID = 316247196L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QMessage message = new QMessage("message");
+
+    public final com.wootech.dropthecode.domain.QBaseEntity _super = new com.wootech.dropthecode.domain.QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    public final QRoom room;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QMessage(String variable) {
+        this(Message.class, forVariable(variable), INITS);
+    }
+
+    public QMessage(Path<? extends Message> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QMessage(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMessage(PathMetadata metadata, PathInits inits) {
+        this(Message.class, metadata, inits);
+    }
+
+    public QMessage(Class<? extends Message> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.room = inits.isInitialized("room") ? new QRoom(forProperty("room"), inits.get("room")) : null;
+    }
+
+}
+

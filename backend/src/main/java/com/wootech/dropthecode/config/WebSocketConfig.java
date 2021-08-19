ad68b6d3e99b961ee204b5f35b21a97dc33523c5
TaewanKimmmm <ktww88@gmail.com>
2021-08-19 19:34:02 +0900

    feat: APIC에서 웹 소켓 테스트 가능하도록 허용

--- backend/src/main/java/com/wootech/dropthecode/config/WebSocketConfig.java
+++ backend/src/main/java/com/wootech/dropthecode/config/WebSocketConfig.java
@@ -25,6 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-connection")
+                .setAllowedOrigins("chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam")
                 .withSockJS();
     }
 }

8623545a16340bc7afba377142a4f35595223d13
shinsehantan <shinsehantan@users.noreply.github.com>
2021-08-25 00:29:02 +0900

    chore: styled-normalize 라이브러리 삭제

--- frontend/src/components/GlobalStyle/GlobalStyle.tsx
+++ frontend/src/components/GlobalStyle/GlobalStyle.tsx
@@ -1,11 +1,9 @@
 import { createGlobalStyle } from "styled-components";
-import normalize from "styled-normalize";
 import reset from "styled-reset";
 
 import { COLOR } from "utils/constants/color";
 
 const GlobalStyle = createGlobalStyle`
-  ${normalize}
   ${reset}
 
   @font-face {

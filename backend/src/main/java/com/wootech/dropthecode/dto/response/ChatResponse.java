636123964a43854cd063763ad8974d80ce098cd4
TaewanKimmmm <ktww88@gmail.com>
2021-09-06 04:06:08 +0900

    feat: ChatResponse 필드 추가

--- backend/src/main/java/com/wootech/dropthecode/dto/response/ChatResponse.java
+++ backend/src/main/java/com/wootech/dropthecode/dto/response/ChatResponse.java
@@ -17,9 +17,29 @@ public class ChatResponse {
     private Long senderId;
 
     /**
+     * 송신자 닉네임
+     */
+    private String senderName;
+
+    /**
+     * 송신자 프로필 이미지
+     */
+    private String senderImageUrl;
+
+    /**
      * 수신자 아이디
      */
     private Long receiverId;
+
+    /**
+     * 수신자 닉네임
+     */
+    private String receiverName;
+
+    /**
+     * 수신자 프로필 이미지
+     */
+    private String receiverImageUrl;
 
     /**
      * 메시지 내용
@@ -32,18 +52,27 @@ public class ChatResponse {
     private LocalDateTime createdAt;
 
     @Builder
-    public ChatResponse(Long senderId, Long receiverId, String message, LocalDateTime createdAt) {
+    public ChatResponse(Long senderId, String senderName, String senderImageUrl,
+                        Long receiverId, String receiverName, String receiverImageUrl,
+                        String message, LocalDateTime createdAt) {
         this.senderId = senderId;
+        this.senderName = senderName;
+        this.senderImageUrl = senderImageUrl;
         this.receiverId = receiverId;
+        this.receiverName = receiverName;
+        this.receiverImageUrl = receiverImageUrl;
         this.message = message;
         this.createdAt = createdAt;
     }
 
-
     public static ChatResponse from(Chat chat) {
         return ChatResponse.builder()
                            .senderId(chat.getSender().getId())
+                           .senderName(chat.getSender().getName())
+                           .senderImageUrl(chat.getSender().getImageUrl())
                            .receiverId(chat.getReceiver().getId())
+                           .receiverName(chat.getReceiver().getName())
+                           .receiverImageUrl(chat.getReceiver().getImageUrl())
                            .message(chat.getContent())
                            .createdAt(chat.getCreatedAt()).build();
     }

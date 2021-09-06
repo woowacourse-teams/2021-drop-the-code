f5cbbb1ab1603c0f65b46654faaf9cca62c35af7
TaewanKimmmm <ktww88@gmail.com>
2021-09-06 13:38:58 +0900

    refactor: 파라미터 순서 변경

--- backend/src/main/java/com/wootech/dropthecode/repository/RoomRepository.java
+++ backend/src/main/java/com/wootech/dropthecode/repository/RoomRepository.java
@@ -8,6 +8,6 @@ import com.wootech.dropthecode.domain.chatting.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    Optional<Room> findByTeacherIdAndStudentId(Long studentId, Long teacherId);
+    Optional<Room> findByTeacherIdAndStudentId(Long teacherId, Long studentId);
     List<Room> findByTeacherIdOrStudentId(Long teacher_id, Long student_id);
 }

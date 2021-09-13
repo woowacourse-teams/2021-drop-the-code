1b858f3b820541e547343cd7addfeda1587cd8f9
TaewanKimmmm <ktww88@gmail.com>
2021-09-14 02:32:28 +0900

    fix: 역할과 상관없이 두 멤버가 하나의 채팅방을 사용하도록 수정

--- backend/src/main/java/com/wootech/dropthecode/repository/RoomRepository.java
+++ backend/src/main/java/com/wootech/dropthecode/repository/RoomRepository.java
@@ -6,8 +6,15 @@ import java.util.Optional;
 import com.wootech.dropthecode.domain.chatting.Room;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    Optional<Room> findByTeacherIdAndStudentId(Long teacherId, Long studentId);
+
+    @Query("select r from Room r " +
+            "where (r.teacher.id = :teacherId and r.student.id = :studentId) " +
+            "or (r.teacher.id = :studentId and r.student.id = :teacherId)")
+    Optional<Room> findByTeacherIdAndStudentId(@Param("teacherId") Long teacherId, @Param("studentId") Long studentId);
+
     List<Room> findByTeacherIdOrStudentId(Long teacherId, Long studentId);
 }
